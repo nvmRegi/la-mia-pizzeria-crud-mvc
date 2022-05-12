beabe97e911f0a0d4ad7b21d6df03bf4bfdf40ba
regi <cabael.regine@gmail.com>
2022-05-12 17:01:55 +0200

    Add project files.

---
+++ La Mia pizzeria static/Controllers/MenùController.cs
@@ -0,0 +1,64 @@
+﻿using La_Mia_pizzeria_static.Models;
+using Microsoft.AspNetCore.Mvc;
+using La_Mia_pizzeria_static.Utils;
+
+namespace La_Mia_pizzeria_static.Controllers
+{
+    public class MenùController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            List<Pizza> pizze = MenùData.GetPizze();
+            return View(pizze);
+        }
+
+        [HttpGet]
+        public IActionResult Dettagli(string nome)
+        {
+            Pizza pizzaFound = null;
+
+            foreach(Pizza pizza in MenùData.GetPizze())
+            {
+                if(pizza.Nome == nome)
+                {
+                    pizzaFound = pizza;
+                    break;
+                }
+            }
+
+            if(pizzaFound != null)
+            {
+                return View("Dettagli", pizzaFound);
+            }
+            else
+            {
+                return NotFound("La pizza " + nome + "non è stato trovata");
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View("FormPizza");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Pizza nuovaPizza)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View("FormPost", nuovaPizza);
+            }
+
+            Pizza nuovaPizzaConNome = new Pizza(nuovaPizza.Nome, nuovaPizza.Ingrediente, nuovaPizza.Image, nuovaPizza.Prezzo);
+            
+            MenùData.GetPizze().Add(nuovaPizzaConNome);
+
+            return RedirectToAction("Index");
+        }
+    }
+
+
+}
---
+++ La Mia pizzeria static/Models/Pizza.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace La_Mia_pizzeria_static.Models
+{
+    public class Pizza
+    {
+        [Required(ErrorMessage ="Il campo nome è obbligatorio")]
+        [StringLength(20, ErrorMessage ="Il nome non può avere più di 20 caratteri")]
+        public string Nome { get; set; }
+        
+        [Required(ErrorMessage ="Il campo ingredienti è obbligatorio")]
+        public string Ingrediente { get; set; }
+        
+        [Required(ErrorMessage ="L'URL dell'immagine è obbligatoria")]
+        public string Image { get; set; }
+        public double Prezzo { get; set; }
+
+        public Pizza()
+        {
+
+        }
+
+        public Pizza(string nome, string ingrediente, string image, double prezzo)
+        {
+            Nome = nome;
+            Ingrediente = ingrediente;
+            Image = image;
+            Prezzo = prezzo;
+        }
+    }
+}
