338618376b858a654ce57344f1aeb92fe77808a6
regi <cabael.regine@gmail.com>
2022-05-13 14:19:39 +0200

    Link footer

--- La Mia pizzeria static/Utils/MenùData.cs
+++ La Mia pizzeria static/Utils/MenùData.cs
@@ -14,13 +14,13 @@ namespace La_Mia_pizzeria_static.Utils
             }
             List<Pizza> nuovaListaPizza = new List<Pizza>();
             //Pizze default
-            Pizza pizza = new Pizza("Marinara", "Pomodoro - origano - aglio - olio", "./img/pizza-marinara.jpg", 4);
+            Pizza pizza = new Pizza("Marinara", "Pomodoro - origano - aglio - olio", "pizza-marinara.jpg", 4);
             nuovaListaPizza.Add(pizza);
 
-            Pizza pizza1 = new Pizza("Margherita", "Pomodoro - mozzarella", "./img/pizza-g7672da356_640.jpg", 5);
+            Pizza pizza1 = new Pizza("Margherita", "Pomodoro - mozzarella", "pizza-g7672da356_640.jpg", 5);
             nuovaListaPizza.Add(pizza1);
 
-            Pizza pizza2 = new Pizza("Funghi", "Pomodoro - mozzarella - funghi champignon", "./img/pizza-funghi.jpg", 6);
+            Pizza pizza2 = new Pizza("Funghi", "Pomodoro - mozzarella - funghi champignon", "pizza-funghi.jpg", 6);
             nuovaListaPizza.Add(pizza2);
 
             MenùData.pizze = nuovaListaPizza;
